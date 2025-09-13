f0ffb7868e5a30bd04ac99793f38d4dfe89e4643
Aiza Humayun <123739993+aizahumayun@users.noreply.github.com>
2025-09-13 14:28:08 -0700

    changws

--- src/App1.jsx
+++ src/App1.jsx
@@ -7,9 +7,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div class="bg-green-700 w-90 h-58 size-72 justify-center flex">
-      <div className="bg-red-700 rounded-full m-8 -ml-12 mt-5 size-12 w-1/2 h-45"></div>
-         
+    <div class="bg-yellow-500 w-90 h-58 flex">
+      <div className="bg-blue-800 w-1/3"></div>
+      <div className="bg-white w-1/3"></div>
+      <div className="bg-red-500 w-1/3"></div>    
     </div>
   )
   
